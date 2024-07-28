a84a84044bdbd8677284b1598677671ad3ec4866
AbdulKareemios <a.kareem.tn@gmail.com>
2024-07-28 14:29:36 +0500

    Initial Commit

---
+++ CaptureSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CaptureSampleApp
+//
+//  Created by AK on 7/28/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
